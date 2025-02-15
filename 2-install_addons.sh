2b8ba134c6b541bf3ce7cc6fe2c1ef5f69d0ae79
Thanabodin19 <Thanabodin.kea@gmail.com>
2025-02-15 16:44:48 +0000

    update cluster and addons

--- 2-install_addons.sh
+++ 2-install_addons.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-# ตั้งค่า Namespace
-ARGOCD_NAMESPACE="argocd"
-MONITORING_NAMESPACE="monitoring"
-METALLB_NAMESPACE="metallb"
-NGINX_NAMESPACE="ingress-nginx"
-
 echo "🚀 Installing ArgoCD using Helm..."
 # เพิ่ม Helm repository 
 helm repo add argo-cd https://argoproj.github.io/argo-helm
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add metallb https://metallb.github.io/metallb
+helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
 # ติดตั้ง Addons ผ่าน Helm
-helm upgrade --install argocd argo-cd/argo-cd  --version 7.8.2 --namespace $ARGOCD_NAMESPACE --create-namespace
-helm upgrade --install kube-prometheus-stack prometheus-community/kube-prometheus-stack --version 69.2.3 --namespace $MONITORING_NAMESPACE --create-namespace
-# helm upgrade --install metallb metallb/metallb --version 0.14.9 --namespace $METALLB_NAMESPACE --create-namespace
+helm upgrade --install argocd argo-cd/argo-cd  --version 7.8.2 --namespace=argocd --create-namespace
+helm upgrade --install kube-prometheus-stack prometheus-community/kube-prometheus-stack --version 69.2.3 --namespace=monitoring --create-namespace
+helm upgrade --install loki  grafana/loki-stack --namespace=loki-stack --create-namespace
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+kubectl apply -f https://kind.sigs.k8s.io/examples/ingress/deploy-ingress-nginx.yaml
+kubectl apply -f https://gitlab.com/settakit.sirisoft/k8s-101-workshop/-/raw/master/workshop/metric-server/metric-server.yaml
 
 echo "🎉 Addons installation completed!"
